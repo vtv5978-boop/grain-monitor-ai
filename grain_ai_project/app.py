7dca2678e7b63ef6ac11c787842ec1cfda54e01e
vtv5978-boop <vtv5978@gmail.com>
2026-06-15 02:05:39 +0500

    app.py

--- grain_ai_project/app.py
+++ grain_ai_project/app.py
@@ -1,8 +1,8 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def index():
     return render_template("index.html")
 
